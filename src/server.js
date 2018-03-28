1e886b51c3723b460e7c10f738f41a99fb5f5d4b
zerolhao <zerolhao@gmail.com>
2018-03-29 01:37:28 +0800

    添加 node server 测试

--- src/server.js
+++ src/server.js
@@ -2,17 +2,18 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
+var qiniu = require('qiniu')
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,23 +22,30 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path==='/uptoken'){
+  if (path === '/uptoken') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/json;charset=utf-8')
-    response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001')
+    response.setHeader('Access-Control-Allow-Origin', '*')
+
     var key = fs.readFileSync('./qiniu-key.json')
     key = JSON.parse(key)
+    
+    let { accessKey, secretKey } = key
+    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 
-    let {accessKey, sertKey} = key
-    var mac
+    var options = {
+      scope: "wy-music",
+    };
+    var putPolicy = new qiniu.rs.PutPolicy(options);
+    var uploadToken = putPolicy.uploadToken(mac);
 
     response.write(`
     {
-      "uptoken": ${}
+      "uptoken": ${uploadToken}
     }
     `)
     response.end()
-  }else{
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
@@ -51,8 +59,8 @@ var server = http.createServer(function(request, response){
   /******** 代码结束，下面不要看 ************/
 })
 
-function readBody(request){
-  return new Promise((resolve, reject)=>{
+function readBody(request) {
+  return new Promise((resolve, reject) => {
     let body = []
     request.on('data', (chunk) => {
       body.push(chunk);
@@ -65,5 +73,3 @@ function readBody(request){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
