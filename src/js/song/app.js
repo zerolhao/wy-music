1e7fe32477d0855466d1ce09b0e909865e381a32
zerolhao <zerolhao@gmail.com>
2018-04-04 20:06:40 +0800

    add 播放和暂停

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,5 +1,28 @@
 {
-  let view = {}
+  let view = {
+    el: '#app',
+    init(){
+      this.$el = $(this.el)
+    },
+    template: `
+      <audio controller src="{{url}}"></audio>
+      <div>
+        <button class="play">播放</button>
+        <button class="pause">暂停</button>
+      </div>
+    `,
+    render(data){
+      this.$el.html(this.template.replace('{{url}}',data.url))
+    },
+    play(){
+      let audio = this.$el.find('audio')[0]
+      audio.play()
+    },
+    pause(){
+      let audio = this.$el.find('audio')[0]
+      audio.pause()
+    }
+  }
   let model = {
     data: { id: '', name: '', singer: '', url: '' },
     get(id) {
@@ -7,16 +30,27 @@
       return query.get(id).then((song)=>{
         Object.assign(this.data, {id:song.id, ...song.attributes})
         return song
-      });
+      })
     }
   }
   let controller = {
     init(view, model) {
       this.view = view
       this.model = model
+      this.view.init()
       let id = this.getSongId()
       this.model.get(id).then(()=>{
-        console.log(this.model.data)
+        this.view.render(this.model.data)
+        //this.view.play()
+      })
+      this.bindEvents()
+    },
+    bindEvents(){
+      this.view.$el.on('click', '.play', ()=>{
+        this.view.play()
+      })
+      this.view.$el.on('click', '.pause', ()=>{
+        this.view.pause()
       })
     },
     getSongId() {
