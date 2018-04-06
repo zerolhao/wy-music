393870cc1305a771963b71ea030272661b57851e
zerolhao <zerolhao@gmail.com>
2018-04-06 21:38:53 +0800

    添加页面内容

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -4,15 +4,8 @@
     init(){
       this.$el = $(this.el)
     },
-    template: `
-      <audio controller src="{{url}}"></audio>
-      <div>
-        <button class="play">播放</button>
-        <button class="pause">暂停</button>
-      </div>
-    `,
     render(data){
-      this.$el.html(this.template.replace('{{url}}',data.url))
+      this.$el.find('audio').attr('src',data.song.url)
     },
     play(){
       let audio = this.$el.find('audio')[0]
@@ -24,11 +17,13 @@
     }
   }
   let model = {
-    data: { id: '', name: '', singer: '', url: '' },
-    get(id) {
+    data: { 
+      song: {id: '', name: '', singer: '', url: ''}
+     },
+    getSong(id) {
       var query = new AV.Query('Song');
       return query.get(id).then((song)=>{
-        Object.assign(this.data, {id:song.id, ...song.attributes})
+        Object.assign(this.data.song, {id:song.id, ...song.attributes})
         return song
       })
     }
@@ -39,17 +34,19 @@
       this.model = model
       this.view.init()
       let id = this.getSongId()
-      this.model.get(id).then(()=>{
+      this.model.getSong(id).then(()=>{
         this.view.render(this.model.data)
         //this.view.play()
       })
       this.bindEvents()
     },
     bindEvents(){
-      this.view.$el.on('click', '.play', ()=>{
+      this.view.$el.on('click', '.icon-play', ()=>{
+        this.view.$el.find('.disc-container').addClass('playing')
         this.view.play()
       })
-      this.view.$el.on('click', '.pause', ()=>{
+      this.view.$el.on('click', '.icon-pause', ()=>{
+        this.view.$el.find('.disc-container').removeClass('playing')        
         this.view.pause()
       })
     },
